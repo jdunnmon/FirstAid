b1d4d6be8bbb4a1d18c92a10db1311bc63f349ea
jdunnmon <jdunnmon@gmail.com>
2017-11-14 11:15:11 -0800

    added other shells

---
+++ shell/run_all_sing.sh
@@ -0,0 +1,49 @@
+EXEC_SCRIPT=../train_CNNclassification.py
+TRAIN_PATH_ROOT="/home/jdunnmon/research/re/projects/tanda/data/data_aug/ddsm-data/ann_preproc/h5_data_all/"
+DATA_SUF="_all"
+PATH_TRAIN="$TRAIN_PATH_ROOT/h5_train_set$DATA_SUF"
+PATH_VAL="$TRAIN_PATH_ROOT/h5_val_set$DATA_SUF"
+PATH_TEST="$TRAIN_PATH_ROOT/h5_test_set$DATA_SUF"
+OUTPUT_PATH="/home/jdunnmon/research/re/projects/tanda/data/data_aug/runlogs"
+
+CUDA_VISIBLE_DEVICES=0
+
+NET_NAME=GoogLe
+EPOCHS=20
+
+EXP_NAME=hp_search_${NET_NAME}
+START_DATE=`date +"%m_%d_%y"`
+
+for lr in 0.001 
+do
+for dp in 0.9
+do
+for l2 in 0.0001
+do
+for dec in 0.99
+do
+for l1 in 0
+do
+for bs in 32
+do
+  echo "Running Case with LR = $lr, L2= $l2, DO = $dp, DEC= $dec, l1=$l1, BS=$bs"
+  TRIAL_NAME=${EXP_NAME}_ep_${EPOCHS}_lr_${lr}_dp_${dp}_l2_${l2}_dec_${dec}_l1_${l1}_bs_$bs
+  TIME=`date +"%H_%M_%S"`
+  LOGDIR="$OUTPUT_PATH/${START_DATE}/${EXP_NAME}/${TRIAL_NAME}"
+  mkdir -p $LOGDIR
+  PATH_SAVE="$LOGDIR/model"
+  PATH_VIS="$LOGDIR/vis.png"
+  LOGFILE="$LOGDIR/terminal_run_log_${TIME}.log"
+  PATH_LOG="$LOGDIR/internal_run_log_${TIME}.log"
+  echo "Saving log to '$LOGFILE'"
+
+  #source set_env.sh
+  python $EXEC_SCRIPT --pTrain $PATH_TRAIN --pVal $PATH_VAL --pTest $PATH_TEST --pModel $PATH_SAVE --pVis $PATH_VIS --pLog $PATH_LOG --name $EXP_NAME --net $NET_NAME --nClass 2 --nGPU 2 --lr $lr --dec $dec --do $dp --l2 $l2 --l1 $l1 --bs $bs --ep $EPOCHS --time 1440 --bLo 0 --bDisp 0 2>&1 | tee $LOGFILE
+ done
+ done
+ done
+ done
+ done
+ done
+ 
+  
