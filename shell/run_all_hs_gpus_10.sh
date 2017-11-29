98ba7486cd3f935345d76607dcb605cfa4827e3a
jdunnmon <jdunnmon@raiders2.stanford.edu>
2017-11-29 15:04:26 -0800

    added shell scripts

--- shell/run_all_hs_gpus_10.sh
+++ shell/run_all_hs_gpus_10.sh
@@ -1,5 +1,4 @@
-EXEC_SCRIPT=../train_CNNclassification.py
-#TRAIN_PATH_ROOT="/home/jdunnmon/research/re/projects/tanda/data/data_aug/ddsm-data/ann_preproc/h5_data_all/"
+EXEC_SCRIPT=/lfs/local/0/jdunnmon/repos/FirstAid/train_CNNclassification.py
 TRAIN_PATH_ROOT="/lfs/local/0/jdunnmon/data_aug/ddsm-data/ann_preproc/h5_data_all/"
 DATA_SUF="_all"
 PATH_TRAIN="$TRAIN_PATH_ROOT/h5_train_set$DATA_SUF"
@@ -7,27 +6,26 @@ PATH_VAL="$TRAIN_PATH_ROOT/h5_val_set$DATA_SUF"
 PATH_TEST="$TRAIN_PATH_ROOT/h5_test_set$DATA_SUF"
 OUTPUT_PATH="/lfs/local/0/jdunnmon/data_aug/firstaid/all_runs/logs"
 
-CUDA_VISIBLE_DEVICES=0
+CUDA_VISIBLE_DEVICES=0,1
+NET_NAME=Res
+EPOCHS=200
+EXP_NAME=hp_search_${NET_NAME}
 
-NET_NAME=Alex
-EPOCHS=30
-
-EXP_NAME=hp_search_${NET_NAME}_optim_test
 START_DATE=`date +"%m_%d_%y"`
 
-for opt in rmsprop
+for opt in adam rmsprop
 do
-for lr in 0.001 
+for lr in 0.0001 0.001 0.01 
 do
-for dp in 0.9
+for dp in 1.0
 do
-for l2 in 0.0001
+for l2 in 0.001 0.000001
 do
-for dec in 0.99
+for dec in 0.99 
 do
 for l1 in 0
 do
-for bs in 64
+for bs in 16
 do
   echo "Running Case with OPT = $opt, LR = $lr, L2= $l2, DO = $dp, DEC= $dec, l1=$l1, BS=$bs"
   TRIAL_NAME=${EXP_NAME}_ep_${EPOCHS}_opt_${opt}_lr_${lr}_dp_${dp}_l2_${l2}_dec_${dec}_l1_${l1}_bs_${bs}
@@ -49,4 +47,10 @@ do
  done
  done
  done
+
+SCRIPT_PATH=${0}
+SCRIPT_OUT_PATH="$OUTPUT_PATH/${START_DATE}/${EXP_NAME}"
+cp -r $SCRIPT_PATH $SCRIPT_OUT_PATH
+echo "Run complete. This script copied to $SCRIPT_OUT_PATH"
+
   
